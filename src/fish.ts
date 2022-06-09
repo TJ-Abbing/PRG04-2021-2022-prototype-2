8c4ecf27e6198215a176303f155a858d421c2bf7
TJ-Abbing <tjabbing@gmail.com>
2022-06-09 11:54:00 +0200

    Added keyboard movement

---
+++ src/fish.ts
@@ -0,0 +1,23 @@
+import * as PIXI from 'pixi.js'
+import { Game } from './game';
+export class Fish extends PIXI.Sprite{
+    private game: Game;
+    constructor(texture: PIXI.Texture, game: Game){
+        super(texture); // new PIXI.Sprite()
+        this.game = game;
+        this.anchor.set(0.5);
+        this.y = 100;
+        this.tint = Math.random() * 0xFF0000;
+        this.x = Math.random() * 800;
+        this.y = Math.random() * 450;
+    }
+    public update(delta: number){
+        this.x -= delta * 1;
+        if(this.x < (0 - this.width)){
+            // finish
+            // this.game.finish();
+            this.x = 800;
+            this.y = Math.random() * 450;
+        }
+    }
+}
